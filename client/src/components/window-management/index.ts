d6db7cb329f643d6363dfbc4b061d77e4555c3a1
Jack Musick <jack@gocovi.com>
2026-01-19 20:47:21 -0500

    feat(window-management): add index exports

---
+++ client/src/components/window-management/index.ts
@@ -0,0 +1,7 @@
+// client/src/components/window-management/index.ts
+
+export { WindowDock } from "./WindowDock";
+export { WindowDockItem } from "./WindowDockItem";
+export { WindowOverlay } from "./WindowOverlay";
+export * from "./animations";
+export * from "./types";
